5e51c2ed610aaf8cc82a4bdc51a53d4e3557ee58
Blake Sager <blake.sager@activeprospect.com>
2018-09-01 17:46:07 -0500

    Refactor

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -2,30 +2,18 @@
 package isogram
 
 import (
-	"regexp"
-	"sort"
 	"strings"
+	"unicode"
 )
 
 // IsIsogram returns a boolean indicating
 // whether or not a string is an isogram (no repeating alpha characters)
 func IsIsogram(w string) bool {
-	token := tokenize(w)
-	ss := sort.StringSlice(strings.Split(token, ""))
-	ss.Sort()
-
-	for i, v := range ss {
-		if i > 0 {
-			if v == ss[i-1] {
-				return false
-			}
+	w = strings.ToLower(w)
+	for i, v := range w {
+		if unicode.IsLetter(v) && strings.LastIndex(w, string(v)) != i {
+			return false
 		}
 	}
 	return true
-}
-
-func tokenize(s string) string {
-	s = strings.ToLower(s)
-	reg, _ := regexp.Compile("[^a-z]+")
-	return reg.ReplaceAllString(s, "")
 }
